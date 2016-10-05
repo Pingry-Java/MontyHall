1f4dd589aef06bf3c27112100286313dc5418596
Michael Sun <msun2019@pingry.org>
2016-10-04 22:14:15 -0400

    Addded in a readme file and added in javadoc messages in the main program.

--- MontyHall.java
+++ MontyHall.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class MontyHall
 {
+ /**
+  * This is the main method, it is a computer version of the Monty Hall game show with the same rules
+  *
+  * @param args An array that is irrelevant to the program 
+ */
  public static void main(String[] args)
  {
   int carDoor = (int) (Math.random() * 3 + 1);
@@ -36,12 +41,22 @@ public class MontyHall
 
   
  }
- 
+ /**
+  * This method returns what door is left besides the user door and car door
+  *
+  * @param int1 what number the user door is 
+  * @param int2 what number the car door is 
+  * @return The door that has not been chosen yet 
+ */
  public static int getOtherDoor(int int1, int int2)
  { 
   return 6 - int1 - int2;
  }
- 
+ /**
+  * This method asks if the user would like to switch doors or not.
+  *
+  * @return True if they would like to switch and False otherwise 
+ */
  public static boolean getUserSwap()
  {
   Scanner keyboard = new Scanner(System.in);
@@ -56,7 +71,7 @@ public class MontyHall
   }
  }
  /**
-  *This method calculates and returns a door for the host to reveal as not the Grand Prize Door
+  * This method calculates and returns a door for the host to reveal as not the Grand Prize Door
   *
   * @param invalid1 A door that the host cannot open
   * @param invalid2 A door that the host cannot open (may be the same as invalid1).
@@ -74,7 +89,12 @@ public class MontyHall
   }
   return -1; 
  }
- 
+ /**
+  * This method asks which door the user would like to choose of the three.
+  *
+  * @return Door 1, 2 or 3 depending on which door the user chooses 
+  *
+ */
  public static int getUserDoor()
  {
   Scanner keyboard = new Scanner(System.in);
---
+++ README.txt
@@ -0,0 +1,6 @@
+Author: Michael Sun
+Date of Completion: 10/4/16
+
+Description: 
+This program is a computer version of the Monty Hall game show. It contains the same rules
+and plays out the same way. 
